1f9e14ccc5ae0d39b44ec57169448425292d2207
Yuri Norwood <76186754+yuri-norwood@users.noreply.github.com>
2021-01-21 15:00:32 +1300

    Re-implement shellcheck from travis ci to .build/Makefile

--- .build/config.mk
+++ .build/config.mk
@@ -4,6 +4,12 @@
 
 VIMLINT = bin/vimlint
 
-BIN = ${VIMLINT}
+SHELLCHECK = bin/shellcheck
+
+SHELLCHECK_VERSION = latest
+SHELLCHECK_GIT_URL = https://github.com/koalaman/shellcheck
+SHELLCHECK_RELEASE = ${SHELLCHECK_GIT_URL}/releases/download/${SHELLCHECK_VERSION}/shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz
+
+BIN = ${VIMLINT} ${SHELLCHECK}
 LIB = ${BIN:bin/%=lib/%} lib/vimlparser
 
